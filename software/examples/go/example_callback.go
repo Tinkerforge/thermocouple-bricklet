b912e6e64773a36abc074425b612377227e22e93
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:16:49 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/thermocouple_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Thermocouple Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	t, _ := thermocouple_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	t.RegisterTemperatureCallback(func(temperature int32) {
+		fmt.Printf("Temperature: %d °C\n", float64(temperature)/100.0)
+	})
+
+	// Set period for temperature receiver to 1s (1000ms).
+	// Note: The temperature callback is only called every second
+	//       if the temperature has changed since the last call!
+	t.SetTemperatureCallbackPeriod(1000)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/thermocouple_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Thermocouple Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	t, _ := thermocouple_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get current temperature.
+	temperature, _ := t.GetTemperature()
+	fmt.Printf("Temperature: %f °C\n", float64(temperature)/100.0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
